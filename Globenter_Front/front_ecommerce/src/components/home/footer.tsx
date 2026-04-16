88af7673df393b09512b7a16917dec82d7fa9a7a
najiba-amiri <najiba.amiri1998@gmail.com>
2026-04-16 10:51:22 -0700

    e
    e

--- Globenter_Front/front_ecommerce/src/components/home/footer.tsx
+++ Globenter_Front/front_ecommerce/src/components/home/footer.tsx
@@ -82,6 +82,9 @@ export default function Footer() {
             <li>
               <a href="#" className="hover:text-[#f1a013]">{t("Clothing")}</a>
             </li>
+             <li>
+              <a href="#" className="hover:text-[#f1a013]">{t("Electronics")}</a>
+            </li>
           </ul>
         </div>
       </div>
